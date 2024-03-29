812d6e548ddc3970050f2b3a443ac9ef53328662
mackswang <mackswang@tencent.com>
2024-03-29 15:47:18 +0800

    distributed_train

--- dist_train.py
+++ dist_train.py
@@ -4,9 +4,11 @@ import os
 from datetime import datetime
 
 import torch
+import torch.distributed as dist
 from torch.utils.data import DataLoader
-
+from torch.utils.data.distributed import DistributedSampler
 import wandb
+
 from data_process.mixed_dataset import MixedDataset
 from data_process.qb_dataset import QbDataset
 from data_process.utils import get_unwarp, tensor_unwarping
@@ -57,73 +59,26 @@ def setup_data(args):
         split="train",
     )
     v_UVDoc_data = data_cls(split="val")
+
+    train_sampler = DistributedSampler(t_UVDoc_data)
+    val_sampler = DistributedSampler(v_UVDoc_data)
+
     train_loader = DataLoader(
         dataset=t_UVDoc_data,
+        sampler=train_sampler,
         batch_size=args.batch_size,
         num_workers=args.num_workers,
-        shuffle=True,
         pin_memory=True,
     )
     val_loader = DataLoader(
         dataset=v_UVDoc_data,
+        sampler=val_sampler,
         batch_size=args.batch_size,
         num_workers=args.num_workers,
-        shuffle=True,
         pin_memory=True,
     )
 
-    return train_loader, val_loader
-
-
-def setup_data_v1(args):
-    t_UVDoc_data = QbDataset(
-        appearance_augmentation=args.appearance_augmentation,
-        geometric_augmentations=args.geometric_augmentationsUVDoc,
-        split="train",
-    )
-    v_UVDoc_data = MixedDataset(split="val")
-    train_loader = DataLoader(
-        dataset=t_UVDoc_data,
-        batch_size=args.batch_size,
-        num_workers=args.num_workers,
-        shuffle=True,
-        pin_memory=True,
-    )
-    val_loader = DataLoader(
-        dataset=v_UVDoc_data,
-        batch_size=args.batch_size,
-        num_workers=args.num_workers,
-        shuffle=True,
-        pin_memory=True,
-    )
-
-    return train_loader, val_loader
-
-
-def setup_data_v2(args):
-    data_cls = UVDocDataset[args.data_to_use]
-    t_UVDoc_data = data_cls(
-        appearance_augmentation=args.appearance_augmentation,
-        geometric_augmentations=args.geometric_augmentationsUVDoc,
-        split="train",
-    )
-    v_UVDoc_data = MixedDataset(split="val")
-    train_loader = DataLoader(
-        dataset=t_UVDoc_data,
-        batch_size=args.batch_size,
-        num_workers=args.num_workers,
-        shuffle=True,
-        pin_memory=True,
-    )
-    val_loader = DataLoader(
-        dataset=v_UVDoc_data,
-        batch_size=args.batch_size,
-        num_workers=args.num_workers,
-        shuffle=True,
-        pin_memory=True,
-    )
-
-    return train_loader, val_loader
+    return train_loader, val_loader, train_sampler, val_sampler
 
 
 def get_lr_scheduler(optimizer, args, epoch_start):
@@ -156,6 +111,101 @@ def update_learning_rate(scheduler, optimizer):
     return lr
 
 
+def setup_dist(rank, world_size):
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12345"
+
+    dist.init_process_group(backend="nccl")
+
+
+def cleanup():
+    dist.destroy_process_group()
+
+
+def train_epoch(
+    epoch,
+    train_loader,
+    net,
+    optimizer,
+    lr_scheduler,
+    device,
+    alpha_w,
+    l1_loss,
+    mse_loss,
+):
+    net.train()
+    losscount = 0
+    train_mse = 0.0
+    for batch in train_loader:
+        img_uv, img_uv_dw, bm = batch
+
+        img_uv_c = img_uv.to(device, non_blocking=True)
+        img_uv_dw_c = img_uv_dw.to(device, non_blocking=True)
+        bm_c = bm.to(device, non_blocking=True)
+
+        # net input size is (b, 3, 288, 288)
+        # net output size is (b, 2, 288, 288)
+        pred_bm = net(img_uv_c)
+        if pred_bm.max() > 2:
+            # log(f"warning here: {pred_bm.max()}")
+            pred_bm = ((pred_bm / 288.0) - 0.5) * 2
+        pred_img_dw = tensor_unwarping(img_uv_c, pred_bm)
+
+        optimizer.zero_grad(set_to_none=True)
+
+        recon_loss = l1_loss(pred_img_dw, img_uv_dw_c)
+        bm_loss = l1_loss(pred_bm, bm_c)
+        net_loss = alpha_w * bm_loss + gamma_w * recon_loss
+        net_loss.backward()
+        optimizer.step()
+
+        tmp_mse = mse_loss(pred_img_dw, img_uv_dw_c)
+        wandb.log({"train_loss": net_loss})
+        if losscount % 50 == 0:
+            img_sample = img_uv_c.detach().cpu().numpy()[0].transpose(1, 2, 0)
+            img_dw_sample = img_uv_dw_c.detach().cpu().numpy()[0].transpose(1, 2, 0)
+            img_pred_sample = pred_img_dw.detach().cpu().numpy()[0].transpose(1, 2, 0)
+            image = wandb.Image(img_sample, caption=f"ep{epoch}_before")
+            image_dw = wandb.Image(img_dw_sample, caption=f"after_gt")
+            image_pred = wandb.Image(img_pred_sample, caption=f"after_pred")
+            wandb.log({"examples": [image, image_dw, image_pred]})
+
+        train_mse += float(tmp_mse)
+        losscount += 1
+
+        gc.collect()
+
+    train_mse = train_mse / max(1, losscount)
+    curr_lr = update_learning_rate(lr_scheduler, optimizer)
+    return train_mse, curr_lr
+    # log(f"TRAIN at EPOCH {epoch} ENS: train_mse = {train_mse}, curr_lr = {curr_lr}")
+    # wandb.log({"train_mse": train_mse})
+
+
+def eval_epoch(epoch, val_loader, net, device, mse_loss):
+    net.eval()
+    with torch.no_grad():
+        mse_loss_val = 0.0
+        for batch in val_loader:
+            img, img_dw, bm = batch
+            img = img.to(device)
+            img_dw = img_dw.to(device)
+
+            pred_bm = net(img)
+            if pred_bm.max() > 2:
+                pred_bm = ((pred_bm / 288.0) - 0.5) * 2
+            pred_img_dw = tensor_unwarping(img, pred_bm)
+
+            loss_img_val = mse_loss(img_dw, pred_img_dw)
+            mse_loss_val += float(loss_img_val)
+
+        val_mse = mse_loss_val / len(val_loader)
+
+    return val_mse
+    #     log(f"EVAL at EPOCH {epoch} ENS: val_mse = {val_mse}, curr_lr = {curr_lr}")
+    # wandb.log({"val_mse": val_mse})
+
+
 def load_model(path, net, optimizer, device):
     log("Loading model and optimizer from checkpoint '{}'".format(path))
     checkpoint = torch.load(path, map_location=device)
@@ -167,54 +217,31 @@ def load_model(path, net, optimizer, device):
     return net, optimizer, epoch_start
 
 
-def main_worker(args):
-    train_loader, val_loader = setup_data_v1(args)
-    device = torch.device(f"cuda:{args.id}")
-    torch.cuda.set_device(args.id)
-
-    net = GeoTr()
-    net.to(device)
-
-    l1_loss = torch.nn.L1Loss()
-    mse_loss = torch.nn.MSELoss()
-
-    optimizer = torch.optim.Adam(net.parameters(), lr=args.lr, betas=(0.9, 0.999))
-
-    global gamma_w
-    epoch_start = 0
-
+def basic_worker(args):
+    current_time = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+    experiment_name = "DIST-" + current_time
     if not os.path.exists(args.logdir):
         os.makedirs(args.logdir)
-
-    current_time = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-    experiment_name = (
-        "time_"
-        + current_time
-        + "params"
-        + str(args.batch_size)
-        + "_lr="
-        + str(args.lr)
-        + "_nepochs"
-        + str(args.n_epochs)
-        + "_nepochsdecay"
-        + str(args.n_epochs_decay)
-        + "_alpha"
-        + str(args.alpha_w)
-        + "_gamma="
-        + str(args.gamma_w)
-        + "_gammastartep"
-        + str(args.ep_gamma_start)
-        + "_data"
-        + args.data_to_use
-    )
-
-    if args.resume:
-        experiment_name = "RESUME" + experiment_name
     log_file_name = os.path.join(args.logdir, experiment_name + ".log")
     global logger
     logger = get_logger(log_file_name)
 
-    if args.resume is not None:
+    dist.init_process_group("nccl")
+    rank = dist.get_rank()
+    world_size = dist.get_world_size()
+    device = torch.device(f"cuda:{rank}")
+    torch.cuda.set_device(rank)
+
+    logger.info(f"Start running dist_worker on rank {rank}")
+
+    train_loader, val_loader, train_sampler, val_sampler = setup_data(args)
+    net = GeoTr().to(device)
+    optimizer = torch.optim.Adam(
+        net.parameters(), lr=args.lr * world_size, betas=(0.9, 0.999)
+    )
+    epoch_start = 0
+    global gamma_w
+    if args.resume:
         if os.path.isfile(args.resume):
             net, optimizer, epoch_start = load_model(
                 args.resume, net, optimizer, device
@@ -223,92 +250,43 @@ def main_worker(args):
                 gamma_w = args.gamma_w
         else:
             log("No checkpoint found at '{}'".format(args.resume))
+    net = torch.nn.SyncBatchNorm.convert_sync_batchnorm(net)
+    net = torch.nn.parallel.DistributedDataParallel(net, device_ids=[rank], find_unused_parameters=True)
+
+    l1_loss = torch.nn.L1Loss().to(device)
+    mse_loss = torch.nn.MSELoss().to(device)
 
     lr_scheduler = get_lr_scheduler(optimizer, args, epoch_start)
 
+    dist.barrier()
+
     for epoch in range(epoch_start, args.n_epochs + args.n_epochs_decay + 1):
-        log(f"---- EPOCH {epoch} ----")
-        if epoch == 20:
-            train_loader, val_loader = setup_data_v2(args)
+        best_val_mse = 9999.0
 
-        if epoch >= args.ep_gamma_start:
-            gamma_w = args.gamma_w
-            log(f"at epoch {epoch}, gamma_w changed to {gamma_w}")
+        log(f"---- EPOCH {epoch} for rank {rank} ----")
+        train_sampler.set_epoch(epoch)
+        val_sampler.set_epoch(epoch)
 
-        train_mse = 0.0
-        best_val_mse = 99999.0
-        losscount = 0
-
-        net.train()
-
-        for batch in train_loader:
-            img_uv, img_uv_dw, bm = batch
-
-            img_uv_c = img_uv.to(device, non_blocking=True)
-            img_uv_dw_c = img_uv_dw.to(device, non_blocking=True)
-            bm_c = bm.to(device, non_blocking=True)
-
-            # net input size is (b, 3, 288, 288)
-            # net output size is (b, 2, 288, 288)
-            pred_bm = net(img_uv_c)
-            if pred_bm.max() > 2:
-                # log(f"warning here: {pred_bm.max()}")
-                pred_bm = ((pred_bm / 288.0) - 0.5) * 2
-            pred_img_dw = tensor_unwarping(img_uv_c, pred_bm)
-
-            optimizer.zero_grad(set_to_none=True)
-
-            recon_loss = l1_loss(pred_img_dw, img_uv_dw_c)
-            bm_loss = l1_loss(pred_bm, bm_c)
-            net_loss = args.alpha_w * bm_loss + gamma_w * recon_loss
-            net_loss.backward()
-            optimizer.step()
-
-            tmp_mse = mse_loss(pred_img_dw, img_uv_dw_c)
-            wandb.log({"train_loss": net_loss})
-            if losscount % 50 == 0:
-                img_sample = img_uv_c.detach().cpu().numpy()[0].transpose(1, 2, 0)
-                img_dw_sample = img_uv_dw_c.detach().cpu().numpy()[0].transpose(1, 2, 0)
-                img_pred_sample = (
-                    pred_img_dw.detach().cpu().numpy()[0].transpose(1, 2, 0)
-                )
-                image = wandb.Image(img_sample, caption=f"ep{epoch}_before")
-                image_dw = wandb.Image(img_dw_sample, caption=f"after_gt")
-                image_pred = wandb.Image(img_pred_sample, caption=f"after_pred")
-                wandb.log({"examples": [image, image_dw, image_pred]})
-
-            train_mse += float(tmp_mse)
-            losscount += 1
-
-            gc.collect()
-
-        train_mse = train_mse / max(1, losscount)
-        curr_lr = update_learning_rate(lr_scheduler, optimizer)
+        train_mse, curr_lr = train_epoch(
+            epoch,
+            train_loader,
+            net,
+            optimizer,
+            lr_scheduler,
+            device,
+            args.alpha_w,
+            l1_loss,
+            mse_loss,
+        )
         log(f"TRAIN at EPOCH {epoch} ENS: train_mse = {train_mse}, curr_lr = {curr_lr}")
+
+        val_mse = eval_epoch(epoch, val_loader, net, device, mse_loss)
+        log(f"EVAL at EPOCH {epoch} ENS: val_mse = {val_mse}, curr_lr = {curr_lr}")
+
         wandb.log({"train_mse": train_mse})
-
-        net.eval()
-        with torch.no_grad():
-            mse_loss_val = 0.0
-            for batch in val_loader:
-                img, img_dw, bm = batch
-                img = img.to(device)
-                img_dw = img_dw.to(device)
-
-                pred_bm = net(img)
-                if pred_bm.max() > 2:
-                    pred_bm = ((pred_bm / 288.0) - 0.5) * 2
-                pred_img_dw = tensor_unwarping(img, pred_bm)
-
-                loss_img_val = mse_loss(img_dw, pred_img_dw)
-                mse_loss_val += float(loss_img_val)
-
-            val_mse = mse_loss_val / len(val_loader)
-            log(f"EVAL at EPOCH {epoch} ENS: val_mse = {val_mse}, curr_lr = {curr_lr}")
         wandb.log({"val_mse": val_mse})
-
-        # save best model
-        if val_mse < best_val_mse or epoch == args.n_epochs + args.n_epochs_decay:
+        
+        if rank == 7 and val_mse < best_val_mse or epoch == args.n_epochs + args.n_epochs_decay:
             best_val_mse = val_mse
             # save
             state = {
@@ -321,6 +299,7 @@ def main_worker(args):
                 + f"{current_time}_ep_{epoch + 1}_{val_mse:.5f}_{train_mse:.5f}_best_model.pkl"
             )
             torch.save(state, model_path)
+        dist.barrier()
 
 
 if __name__ == "__main__":
@@ -348,7 +327,7 @@ if __name__ == "__main__":
         help="Dataset to use for training, either 'both' for Doc3D and UVDoc, or 'doc3d' for Doc3D only.",
     )
     parser.add_argument(
-        "--batch_size", nargs="?", type=int, default=48, help="Batch size."
+        "--batch_size", nargs="?", type=int, default=1, help="Batch size."
     )
     parser.add_argument(
         "--n_epochs",
@@ -447,11 +426,15 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
+
+    print(args)
+
     wandb.init(
         project=args.project,
+        group="DDP",
         config={
             "learning_rate": args.lr,
             "epochs": args.n_epochs + args.n_epochs_decay,
         },
     )
-    main_worker(args)
+    basic_worker(args)
