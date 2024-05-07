0f690f8a326a5ed7a6b9e792ffd0d4428dfa5762
mackswang <mackswang@tencent.com>
2024-05-07 20:20:59 +0800

    1

--- data_process/uvdoc_dataset.py
+++ data_process/uvdoc_dataset.py
@@ -22,7 +22,7 @@ def trigger(prob):
 
 def get_bound_crop():
     bound = (0, 448, 0, 448)
-    if trigger(0.8):
+    if trigger(0.4):
         return bound
 
     h, w = randint(2, 3), randint(2, 3)
